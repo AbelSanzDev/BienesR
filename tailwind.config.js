ddd980ebabfc3603c0555f8646efcbccbfed2517
Abel Sánchez <abelsan38@gmail.com>
2024-02-14 18:56:31 -0700

    Se termino index

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 const withMT = require("@material-tailwind/html/utils/withMT");
 
 module.exports = withMT({
-  content: ["./index.html"],
+  content: ["./**/*.html"],  // Incluye todos los archivos HTML en tu proyecto
   theme: {
     extend: {},
   },
   plugins: [],
 });
+
 
